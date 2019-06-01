7cdbf14b5e74d4c15c214d7e99be407e06031a46
League Student <league@iMac-26.attlocal.net>
2019-06-01 11:28:28 -0700

    Photo Quiz, Drum Kit (IP)

--- src/drum_kit/DrumKit.java
+++ src/drum_kit/DrumKit.java
@@ -26,42 +26,82 @@ public class DrumKit implements MouseListener {
 
 		// 1. Make a JFrame variable and initialize it using "new JFrame()"
 
+		JFrame frame = new JFrame();
+
 		// 2. Make the frame visible and
 		// set its default close operation to JFrame.EXIT_ON_CLOSE
 
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 		// 3. Set the size of the frame
+
+		frame.setSize(500, 500);
 
 		// 4. Set the title of the frame
 
+		frame.setTitle("Drums");
+
 		// 5. Make a JPanel variable and initialize it using "new JPanel().
 
+		JPanel panel = new JPanel();
+
 		// 6. Add the panel to the frame. (The panel is invisible.)
+
+		frame.add(panel);
 
 		// 7. Download an image of a drum from the Internet. Drop it into your
 		// Eclipse project under "default package".
 
 		// 8. Put the name of your image file in a String variable.
 
+		String drums = "snare.jpg";
+
 		// 9. Edit the next line to use your String variable
-		// drumLabelWithImage = createLabelImage(drumImageString);
+
+		drumLabelWithImage = createLabelImage("snare.jpg");
 
 		// 10. Add the image to the panel
 
+		panel.add(drumLabelWithImage = createLabelImage("snare.jpg"));
+
 		// 11. Set the layout of the panel to "new GridLayout()"
+
+		panel.setLayout(new GridLayout());
 
 		// 12. call the pack() method on the frame. Run your program. Do you see
 		// your drum image?
 
+		frame.pack();
+
 		// 13. add this mouse listener to drumLabelWithImage
+
+		drumLabelWithImage.addMouseListener(null);
 
 		// 18. Add more images to make a drumkit. Remember to add this mouse
 		// listener to each one.
+		
+		
+		JPanel panel2 = new JPanel();
+		JFrame frame2 = new JFrame();
+		frame2.add(panel2);
+		String drums2 = "drums2.jpg";
+		drumLabelWithImage = createLabelImage("drums2.jpg");
+		panel2.add(drumLabelWithImage);
+		panel2.setLayout(new GridLayout());
+		frame2.pack();
+		drumLabelWithImage.addMouseListener(null);
+		frame2.setVisible(true);
+		frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 
 	}
 
 	public void mouseClicked(MouseEvent e) {
 		// 14. Print "mouse clicked" to the console. Run your program and watch
-		// the console to see when this is printed.
+		// the console to see when this is printed.\
+		
+		System.out.println("mouse clicked");
 
 		JLabel drumClicked = (JLabel) e.getSource(); // This line gets the label
 														// that the mouse
--- src/photo_quiz/PhotoQuiz.java
+++ src/photo_quiz/PhotoQuiz.java
@@ -26,26 +26,62 @@ public class PhotoQuiz {
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
 		// “Copy Image Address”)
+		
+		String pig = "https://cdn.wallpapersafari.com/28/92/w5NUjp.jpg";
 
 		// 2. create a variable of type "Component" that will hold your image
-
+		
+		Component babypig;
+		
 		// 3. use the "createImage()" method below to initialize your Component
+		
+		babypig = createImage(pig);
 
 		// 4. add the image to the quiz window
+		
+		quizWindow.add(babypig);
 
 		// 5. call the pack() method on the quiz window
+		
+		quizWindow.pack();
 
 		// 6. ask a question that relates to the image
+		
+		String question = JOptionPane.showInputDialog("What color is the baby pig?");
 
 		// 7. print "CORRECT" if the user gave the right answer
-
+		
+		if(question.equals("pink")) {
+			System.out.println("correct!");
+		}
+		
 		// 8. print "INCORRECT" if the answer is wrong
-
+		
+		else {
+			System.out.println("wrong!");
+		}
+		
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
-
+		
+		quizWindow.remove(babypig);
+		
 		// 10. find another image and create it (might take more than one line
 		// of code)
+		
+		String cute = "https://i.imgur.com/Nbuz72C.jpg";
+		Component cuteanimal;
+		cuteanimal = createImage(cute);
+		quizWindow.add(cuteanimal);
+		quizWindow.pack();
+		String question2 = JOptionPane.showInputDialog("What animal is this?");
+		if(question2.equals("bunny")) {
+			System.out.println("correct!");
+		}
+		else {
+			System.out.println("wrong!");
+		}
+		
 
 		// 11. add the second image to the quiz window
 
